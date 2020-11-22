92657cf086e7f1cd0f8c8f46b5e40651c111e695
Til Mohr <til.mohr@rwth-aachen.de>
2020-11-22 11:56:14 +0100

    Diff marc updates

--- Versuch 3/Codegeruest/SPOS/SPOS/os_mem_drivers.c
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_mem_drivers.c
@@ -1,16 +1,20 @@
 #include "os_mem_drivers.h"
 
-MemDriver intSRAM__;
+MemDriver intSRAM__ = {
+	.init = initSRAM_internal,
+	.read = readSRAM_internal,
+	.write = writeSRAM_internal,
+	.start = AVR_SRAM_START,
+	.size = AVR_MEMORY_SRAM
+};
 
-void *initSRAM_internal(void) {
-	intSRAM__ = {
-		.init = initSRAM_internal,
-		.read = readSRAM_internal,
-		.write = writeSRAM_internal,
-		.start = AVR_SRAM_START,
-		.size = AVR_MEMORY_SRAM
-	};
-;
-MemValue *readSRAM_internal(MemAddr addr);
-void *writeSRAM_internal(MemAddr addr, MemValue);
+void *initSRAM_internal(void) {};
+	
+MemValue *readSRAM_internal(MemAddr addr) {
+	return *((uint8_t*) addr);
+}
+
+void *writeSRAM_internal(MemAddr addr, MemValue){
+	*((uint8_t*) addr) = value;
+}
 
