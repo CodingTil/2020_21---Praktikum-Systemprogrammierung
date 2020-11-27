e852a0be70fa99746d528d97530129ca665b3dc3
simonmichau <57032947+simonmichau@users.noreply.github.com>
2020-11-27 18:35:14 +0100

    page 15

---
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_mem_drivers.c
@@ -0,0 +1,24 @@
+/*
+ * os_mem_drivers.c
+ *
+ * Created: 26.11.2020 14:28:51
+ *  Author: User
+ */ 
+#include "os_mem_drivers.h"
+
+void init(void){
+	//TODO: initialize storage medium
+	
+}
+
+MemValue read(MemAddr addr){
+	//get value at addr and return
+	return &addr;
+}
+
+void write(MemAddr addr, MemValue value){
+	//store value at addr
+	&addr = value;
+}
+
+struct MemDriver intSRAM__;
---
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_mem_drivers.h
@@ -0,0 +1,29 @@
+/*
+ * os_mem_drivers.h
+ *
+ * Created: 26.11.2020 14:29:23
+ *  Author: User
+ */ 
+
+
+#ifndef OS_MEM_DRIVERS_H_
+#define OS_MEM_DRIVERS_H_
+
+typedef uint16_t MemAddr;
+typedef uint8_t MemValue;
+
+typedef struct{
+	const MemAddr os_mem_drivers_addr;
+	const MemValue os_mem_drivers_value;
+	
+	typedef void (*init)(void);
+	typedef MemValue (*read)(MemAddr);
+	typedef void (*write)(MemAddr, MemValue);
+	
+}MemDriver;
+
+typedef struct MemDriver MemDriver;
+
+
+
+#endif /* OS_MEM_DRIVERS_H_ */
