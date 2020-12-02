b9da4dc6394d8cd47f1e0f55a3f0b3dbe0da4572
Til Mohr <til.mohr@rwth-aachen.de>
2020-12-02 16:20:05 +0100

    No errors, needs testing

--- Versuch 3/Codegeruest/SPOS/SPOS/os_memheap_drivers.c
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_memheap_drivers.c
@@ -1,5 +1,5 @@
 #include "os_memheap_drivers.h"
-#include "os_mem_drivers.h"
+#include "os_mem_drivers.h"const PROGMEM char name[] = "internal";
 
 Heap intHeap__;
 
@@ -9,7 +9,8 @@ void os_initHeaps(void) {
 	intHeap__.map_size = intSRAM->size / 3;
 	intHeap__.use_start = intSRAM->start + intSRAM->size / 3;
 	intHeap__.use_size = intSRAM->size - intSRAM->size / 3;
-	for(MemAddr i = intHeap__.map_size; i < intHeap__.use_start; i++) {
+	intHeap__.name = name;
+	for(MemAddr i = intHeap__.map_start; i < intHeap__.use_start; i++) {
 		intHeap__.driver->write(i, 0);
 	}
 }
