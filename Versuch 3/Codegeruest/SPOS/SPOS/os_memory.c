a2cdb7a788793a901b2159b234cf48e3e45fce12
Marc Ludevid Wulf <marc.ludevid@rwth-aachen.de>
2020-12-03 16:25:02 +0100

    Fixed os_memory.c

--- Versuch 3/Codegeruest/SPOS/SPOS/os_memory.c
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_memory.c
@@ -1,24 +1,73 @@
 #include "os_memory.h"
 #include "os_memory_strategies.h"
 
-MemValue os_getMap(Heap const* heap, MemAddr address) {
-	MemAddr map_address = (address - heap->use_start) / 2 + heap->map_start;
-	// Error when out of bound?
-	if ((address - heap->use_start) % 2 == 0) {
-		return heap->driver->read(map_address) >> 4;
+void setLowNibble(Heap const *heap, MemAddr addr, MemValue value) {
+	heap->driver->write(addr, (value & 0xF) | (heap->driver->read(addr) & 0xF0));
+}
+
+void setHighNibble(Heap const *heap, MemAddr addr, MemValue value) {
+	heap->driver->write(addr, (value << 4) | (heap->driver->read(addr) & 0xF));
+}
+
+MemValue getLowNibble(Heap const *heap, MemAddr addr) {
+	return heap->driver->read(addr) & 0xF;
+}
+
+MemValue getHighNibble(Heap const *heap, MemAddr addr) {
+	return heap->driver->read(addr) >> 4;
+}
+
+MemAddr getMapAddress(Heap const *heap, MemAddr addr) {
+	return (addr - heap->use_start) / 2 + heap->map_start;
+}
+
+void setMapEntry(Heap* heap, MemAddr addr, MemValue value) {
+	MemAddr map_addr = getMapAddress(heap, addr);
+	if ((addr - heap->use_start) % 2 == 0) {
+		setHighNibble(heap, map_addr, value);
 		} else {
-		return heap->driver->read(map_address) & 0xF;
+		setLowNibble(heap, map_addr, value);
 	}
 }
 
-void os_setMap(Heap* heap, MemAddr address, MemValue owner) {
-	MemAddr map_address = (address - heap->use_start) / 2 + heap->map_start;
-	// Error when out of bound?
-	if ((address - heap->use_start) % 2 == 0) {
-		heap->driver->write(map_address, (heap->driver->read(map_address) & 0xF) | (owner << 4));
+MemValue os_getMapEntry(Heap const* heap, MemAddr addr) {
+	MemAddr map_addr = getMapAddress(heap, addr);
+	if ((addr - heap->use_start) % 2 == 0) {
+		return getHighNibble(heap, map_addr);
 		} else {
-		heap->driver->write(map_address, (heap->driver->read(map_address) & 0xF0) | (owner & 0xF));
+		return getLowNibble(heap, map_addr);
 	}
+}
+
+MemAddr os_getFirstByteOfChunk(Heap const* heap, MemAddr addr) {
+	while (os_getMapEntry(heap, addr) == 0xF) addr--;
+	return addr;
+}
+
+ProcessID getOwnerOfChunk(Heap const* heap, MemAddr addr) {
+	return (ProcessID) os_getMapEntry(heap, os_getFirstByteOfChunk(heap, addr));
+}
+
+uint16_t os_getChunkSize(Heap const* heap, MemAddr addr) {
+	MemAddr start = os_getFirstByteOfChunk(heap, addr);
+	if (os_getMapEntry(heap, start) == 0) return 0;
+	uint16_t l = 1;
+	while (os_getMapEntry(heap, start + l) == 0xF) {
+		l++;
+	}
+	return l;
+}
+
+void os_freeOwnerRestricted(Heap* heap, MemAddr addr, ProcessID owner) {
+	os_enterCriticalSection();
+	MemAddr start = os_getFirstByteOfChunk(heap, addr);
+	if ((ProcessID) os_getMapEntry(heap, start) == owner) {
+		do {
+			setMapEntry(heap, start, 0);
+			start++;
+		} while (os_getMapEntry(heap, start) == 0xF);
+	}
+	os_leaveCriticalSection();
 }
 
 MemAddr os_malloc(Heap* heap, uint16_t size) {
@@ -32,38 +81,17 @@ MemAddr os_malloc(Heap* heap, uint16_t size) {
 	}
 	
 	if (address != 0) {
-		os_setMap(heap, address, os_getCurrentProc());
+		setMapEntry(heap, address, os_getCurrentProc());
 		for (uint16_t i = 1; i < size; i++) {
-			os_setMap(heap, address + i, 0xF);
+			setMapEntry(heap, address + i, 0xF);
 		}
 	}
 	os_leaveCriticalSection();
 	return address;
 }
 
-MemAddr os_getFirstAddress(Heap const* heap, MemAddr address) {
-	while (os_getMap(heap, address) == 0xF) address--;
-	return address;
-}
-
-ProcessID os_getOwner(Heap const* heap, MemAddr address) {
-	return os_getMap(heap, address);
-}
-
-void os_free_pid(Heap* heap, MemAddr address, ProcessID pid) {
-	MemAddr start = os_getFirstAddress(heap, address);
-	if (os_getOwner(heap, start) == pid) {
-		do {
-			os_setMap(heap, start, 0);
-			start++;
-		} while (os_getMap(heap, start) == 0xF);
-	}
-}
-
 void os_free(Heap* heap, MemAddr address) {
-	os_enterCriticalSection();
-	os_free_pid(heap, address, os_getCurrentProc());
-	os_leaveCriticalSection();
+	os_freeOwnerRestricted(heap, address, os_getCurrentProc());
 }
 
 size_t os_getMapSize(Heap const* heap) {
@@ -82,18 +110,6 @@ MemAddr os_getUseStart(Heap const* heap) {
 	return heap->use_start;
 }
 
-uint16_t os_getChunkSize(Heap const* heap, MemAddr address) {
-	os_enterCriticalSection();
-	MemAddr start = os_getFirstAddress(heap, address);
-	if (os_getOwner(heap, start) == 0) return 0;
-	uint16_t l = 1;
-	while (os_getOwner(heap, start + l) == 0xF) {
-		l++;
-	}
-	os_leaveCriticalSection();
-	return l;
-}
-
 AllocStrategy os_getAllocationStrategy(Heap const* heap) {
 	return heap->alloc_strategy;
 }
@@ -105,7 +121,7 @@ void os_setAllocationStrategy(Heap* heap, AllocStrategy allocStrat) {
 void os_freeProcessMemory(Heap* heap, ProcessID pid) {
 	os_enterCriticalSection();
 	for (size_t i = heap->use_start; i < heap->use_start + heap->use_size; i++) {
-		os_free_pid(heap, i, pid);
+		os_freeOwnerRestricted(heap, i, pid);
 	}
 	os_leaveCriticalSection();
 }
