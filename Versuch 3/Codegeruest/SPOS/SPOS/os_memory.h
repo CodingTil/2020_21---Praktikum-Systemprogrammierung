9abe88094b8a567a0d5949041c863d5f1dfd4583
Marc Ludevid Wulf <redacted@example.com>
2020-12-03 16:25:02 +0100

    Fixed os_memory.c

--- Versuch 3/Codegeruest/SPOS/SPOS/os_memory.h
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_memory.h
@@ -5,8 +5,12 @@
 #include "os_memheap_drivers.h"
 #include "os_scheduler.h"
 
-MemAddr os_malloc(Heap* heap, uint16_t size);
+MemAddr os_malloc(Heap* heap, size_t size);
 void os_free(Heap* heap, MemAddr addr);
+
+MemValue os_getMapEntry(Heap const *heap, MemAddr addr);
+
+void os_freeProcessMemory(Heap* heap, ProcessID pid);
 
 size_t os_getMapSize(Heap const* heap);
 size_t os_getUseSize(Heap const* heap);
@@ -14,10 +18,10 @@ MemAddr os_getMapStart(Heap const* heap);
 MemAddr os_getUseStart(Heap const* heap);
 
 uint16_t os_getChunkSize(Heap const* heap, MemAddr addr);
+
+MemAddr os_getFirstByteOfChunk(Heap const *heap, MemAddr addr);
+
 AllocStrategy os_getAllocationStrategy(Heap const* heap);
 void os_setAllocationStrategy(Heap* heap, AllocStrategy allocStrat);
-
-ProcessID os_getOwner(Heap const* heap, MemAddr address); //Should this really be public?
-void os_freeProcessMemory(Heap* heap, ProcessID pid);
 
 #endif /* OS_MEMORY_H_ */
