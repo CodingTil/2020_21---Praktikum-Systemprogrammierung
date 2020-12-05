87a486fc6736552abee0c95fe3cd7e2bffb5e228
Marc Ludevid Wulf <marc.ludevid@rwth-aachen.de>
2020-12-05 02:00:44 +0100

    Manually merged master.

--- Versuch 3/Codegeruest/SPOS/SPOS/os_memory_strategies.c
+++ Versuch 3/Codegeruest/SPOS/SPOS/os_memory_strategies.c
@@ -1,5 +1,6 @@
 #include "os_memory_strategies.h"
 #include "os_memory.h"
+#include "os_core.h"
 
 MemAddr os_Memory_FirstFit(Heap *heap, size_t size) {
 	MemAddr addr;
@@ -8,7 +9,7 @@ MemAddr os_Memory_FirstFit(Heap *heap, size_t size) {
 		if (os_getMapEntry(heap, addr) == 0) {
 			current_size++;
 			if (current_size >= size) {
-				return addr - size + 1;
+				return addr - current_size + 1;
 			}
 		} else {
 			current_size = 0;
@@ -18,37 +19,23 @@ MemAddr os_Memory_FirstFit(Heap *heap, size_t size) {
 }
 
 MemAddr os_Memory_NextFit(Heap *heap, size_t size) {
-	static MemAddr last_addr = 0;
-	if (last_addr == 0) {
-		last_addr = os_Memory_FirstFit(heap, size) + size;
-		if (last_addr >= heap->use_start + heap->use_size) last_addr = heap->use_start;
-		return last_addr - size;
-	}
 	MemAddr address;
 	size_t current_size = 0;
-	for (address = last_addr; address < heap->use_start + heap->use_size; address++) {
+	for (address = heap->last_addr; address < heap->use_start + heap->use_size; address++) {
 		if (os_getMapEntry(heap, address) == 0) {
 			current_size++;
 			if (current_size >= size) {
-				last_addr = address + 1;
-				if (last_addr >= heap->use_start + heap->use_size) last_addr = heap->use_start;
-				return address - size + 1;
+				heap->last_addr = address + 1;
+				return address - current_size + 1;
 			}
 		} else {
 			current_size = 0;
 		}
 	}
-	for (address = heap->use_start; address < heap->use_start + heap->use_size; address++) {
-		if (os_getMapEntry(heap, address) == 0) {
-			current_size++;
-			if (current_size >= size) {
-				last_addr = address + 1;
-				if (last_addr >= heap->use_start + heap->use_size) last_addr = heap->use_start;
-				return address - size + 1;
-			}
-		} else {
-			current_size = 0;
-		}
+	address = os_Memory_FirstFit(heap, size);
+	if (address != 0) {
+		heap->last_addr = address + size;
+		return address;
 	}
 	return 0;
 }
@@ -62,15 +49,17 @@ MemAddr os_Memory_BestFit(Heap *heap, size_t size) {
 		if (os_getMapEntry(heap, address) == 0) {
 			current_size++;
 		} else {
-			if (current_size >= size && current_size < best_size) {
-				best_addr = address - size + 1;
+			if(current_size == size) {
+				return address - current_size;	
+			}else if (current_size > size && current_size < best_size) {
+				best_addr = address - current_size;
 				best_size = current_size;
 			}
 			current_size = 0;
 		}
 	}
 	if (current_size >= size && current_size < best_size) {
-		best_addr = address - size + 1;
+		best_addr = address - current_size;
 	}
 	return best_addr;
 }
@@ -84,15 +73,17 @@ MemAddr os_Memory_WorstFit(Heap *heap, size_t size) {
 		if (os_getMapEntry(heap, address) == 0) {
 			current_size++;
 		} else {
-			if (current_size >= size && current_size > best_size) {
-				best_addr = address - size + 1;
+			if(current_size >= size && current_size >= heap->use_size / 2) {
+				return address - current_size;
+			}else if (current_size >= size && current_size > best_size) {
+				best_addr = address - current_size;
 				best_size = current_size;
 			}
 			current_size = 0;
 		}
 	}
 	if (current_size >= size && current_size > best_size) {
-		best_addr = address - size + 1;
+		best_addr = address - current_size;
 	}
 	return best_addr;
 }
