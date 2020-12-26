6e1e5a108129eeccaecf9ebd40e95283f41cc347
Marc Ludevid Wulf <marc.ludevid@gmail.com>
2020-12-26 19:29:31 +0100

    Next session. Memory still doesn't work.

--- Versuch 4/Codegeruest/SPOS/SPOS/os_core.c
+++ Versuch 4/Codegeruest/SPOS/SPOS/os_core.c
@@ -139,7 +139,16 @@ void os_init(void) {
 
 	os_initHeaps();
 
-    os_initScheduler();
+    //os_initScheduler();
+	
+	extHeap->driver->write(12, 12);
+	uint8_t x = extHeap->driver->read(12);
+	
+	lcd_writeDec(x);
+	
+	delayMs(1000);
+	
+	os_error("FINISHED");
 
     os_coarseSystemTime = 0;
 }
